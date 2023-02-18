621e1b77dfc833878d51a38842191326f6103ebe
Jacob Errington <git@mail.jerrington.me>
2023-02-18 09:56:05 -0500

    add oset: a module for ordered sets
    These sets are not ordered in the sense that the _elements_ have some
    kind of ordering, but as with lists, there is an arbitrary order imposed
    on the elements, and elements are associated to _indices_ in the set.

---
+++ oSet.ml
@@ -0,0 +1,28 @@
+(** An ordered set. *)
+type 'a t = 'a list
+
+let rec insert x = function
+  | [] -> [x]
+  | y :: ys when x = y -> y :: ys
+  | y :: ys -> y :: insert x ys
+
+let insert_index x = function
+  | [] -> ([x], 1)
+  | y :: ys when x = y -> (y :: ys, 0)
+  | y :: ys -> let ys, i = insert_index x ys in (y :: ys, i + 1)
+
+let empty = []
+let singleton x = [x]
+
+let of_list l = List.fold_right insert empty l
+
+let to_list s = s
+
+let union s1 s2 = match s1 with
+  | [] -> s2
+  | x :: s1' -> union s1' (insert x s2)
+
+let unions ss = List.fold_right union ss []
+
+(** delete s l removes all elements of l from s. *)
+let delete s l = List.filter (fun x -> not @@ List.mem x l) s
---
+++ oSet.mli
@@ -0,0 +1,29 @@
+(* The type of a set whose elements have an associated index, as in a list. *)
+type 'a t
+
+(* An empty set. *)
+val empty : 'a t
+
+(* A singleton set. *)
+val singleton : 'a -> 'a t
+
+(* Inserts an element into the set. *)
+val insert : 'a -> 'a t -> 'a t
+
+(* Inserts an element into the set giving its index. *)
+val insert_index : 'a -> 'a t -> 'a t * int
+
+(* Computes the union of two sets. *)
+val union : 'a t -> 'a t -> 'a t
+
+(* Computes the union of a list of sets. *)
+val unions : 'a t list -> 'a t
+
+(* Set difference: `delete s1 s2` removes every item in s1 that appears in s2. *)
+val delete : 'a t -> 'a t -> 'a t
+
+(* Turns a set into a list. *)
+val to_list : 'a t -> 'a list
+
+(* Turns a list into a set. This will deduplicate the elements of the list. *)
+val of_list : 'a list -> 'a t
